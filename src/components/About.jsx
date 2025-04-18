f79f216b2de1e33e52e10ba21458f7cb0b0f0a3f
Juma Michael <obudho76@gmail.com>
2025-04-18 09:50:33 +0300

    fifth commit

---
+++ src/components/About.jsx
@@ -0,0 +1,60 @@
+import NavBar from "./NavBar";
+
+export default function About() {
+  return (
+    <>
+      <h1>About us</h1>
+      <NavBar />
+      <div className="card text-center max-w-5xl mx-auto px-4 py-4">
+        <h2 className="text-3xl font-bold mb-4">
+          Welcome to the Livestock Disease Reasearch
+        </h2>
+        <p>
+          We are dedicated to providing valuable insights, data, and resources
+          on diseases that affect livestock. Our goal is to aid farmers,
+          veterinarians, and researchers in understanding and addressing the
+          challenges of animal health.
+        </p>
+      </div>
+      <div className="card text-center max-w-5xl mx-auto px-4 py-4">
+        <h2 className="text-2xl font-semibold mb-2">Why It Matters</h2>
+        <p className="text-gray-700">
+          Livestock diseases can cause devastating economic losses and impact
+          rural livelihoods. Through targeted research and field studies, we aim
+          to develop effective diagnostics, treatment protocols, and prevention
+          strategies.
+        </p>
+      </div>
+      <div className="card text-center max-w-5xl mx-auto px-4 py-4">
+        <h2 className="text-2xl font-semibold mb-2">Our Mission</h2>
+        <p className="text-gray-700">
+          Our mission is to contribute to the betterment of livestock health
+          through research and data collection. By compiling detailed
+          information on various diseases, symptoms, and treatments, we aim to
+          provide a comprehensive resource for livestock care and management.
+        </p>
+      </div>
+      <div className="card text-center max-w-5xl mx-auto px-4 py-4">
+        <h2 className="text-2xl font-semibold mb-2">What We Do</h2>
+        <p className="text-gray-700">
+          We collect and organize information on livestock diseases from a
+          variety of trusted sources, offering:
+           . Disease Name and Information 
+           .Symptoms
+            . Treatments and Prevention Method 
+            . Research Articles and Resources
+        </p>
+      </div>
+      <div className="card text-center max-w-5xl mx-auto px-4 py-4">
+        <h2 className="text-2xl font-semibold mb-2">How We Can Help You</h2>
+        <p className="text-gray-700">
+          Our platform allows users to search for diseases by name, animal type,
+          and symptoms, making it easier for farmers and veterinarians to find
+          the information they need. Additionally, our user-friendly forms allow
+          new disease data to be added, helping to keep the information current
+          and valuable to the community.
+        </p>
+      </div>
+    </>
+  );
+}
